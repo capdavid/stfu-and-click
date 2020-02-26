a62b8a98d592ff92de21ed1831b5ae3798104f0d
David Čáp <capdavidd@gmail.com>
2020-02-26 22:14:03 +0100

    refactor: changed to HTML5 elements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,19 +4,19 @@ import styled from '../styles/styled';
 import Text from './Text';
 
 const Footer: React.FC = () => {
-    const StyledFooter = styled.div`
-        position: absolute;
-        bottom: 0;
-        margin: 1rem auto;
-        width: 100%;
-    `;
+  const StyledFooter = styled.footer`
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 3rem;
+  `;
 
-    return (
-        <StyledFooter>
-            <Text>
-                If you don't like this page, it's <a href="https://applifting.io">Applifting's</a> fault.
-            </Text>
-        </StyledFooter>
-    );
+  return (
+    <StyledFooter>
+      <Text>
+        If you don't like this page, it's <a href="https://applifting.io">Applifting's</a> fault.
+      </Text>
+    </StyledFooter>
+  );
 };
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import styled from '../styles/styled';
 
-const Header = styled.p`
-    margin: 0;
-    width: 100%;
-    padding: 1rem;
-    text-align: center;
-    font-size: 1.2rem;
-    font-weight: 900;
-    background: ${p => p.theme.colors.primary};
-    color: ${p => p.theme.colors.white};
+const Header = styled.header`
+  margin: 0;
+  width: 100%;
+  padding: 0.5rem;
+  text-align: center;
+  font-family: 'Montserrat', 'Roboto';
+  font-size: 1.5rem;
+  font-weight: 900;
+  background: ${p => p.theme.colors.primary};
+  color: ${p => p.theme.colors.white};
 `;
 
 export default Header;
