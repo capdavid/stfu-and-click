6dede036ef439148e77556fcc1d01570b14bd197
David Čáp <capdavidd@gmail.com>
2020-03-04 23:10:31 +0100

    feat: changed title to STFUANDCLICK & updated props on Text component

--- src/components/UI/Text.tsx
+++ src/components/UI/Text.tsx
@@ -4,7 +4,6 @@ interface TextProps {
   left?: boolean;
   withPadding?: boolean;
   inline?: boolean;
-  green?: boolean;
 }
 
 const Text = styled.p<TextProps>`
@@ -13,7 +12,6 @@ const Text = styled.p<TextProps>`
   text-align: ${props => (props.left ? 'left' : 'center')};
   margin: 0 auto;
   font-style: italic;
-  color: ${props => (props.green ? 'green' : 'inherit')};
 `;
 
 export default Text;
