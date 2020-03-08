084d690ddfbccdb7536c4db6657132395d2cc951
David Čáp <capdavidd@gmail.com>
2020-03-08 21:20:58 +0100

    feat: team name can't start or end with a space

--- src/components/validateTeamRegister.tsx
+++ src/components/validateTeamRegister.tsx
@@ -2,14 +2,13 @@ export default function validate(value: string) {
   console.log(value);
   let error = '';
   if (!value) {
-    console.log('empty string');
     error = 'Please enter a team name.';
   } else if (value.length > 20) {
-    console.log('too long');
     error = 'Team name must be less than 20 characters.';
   } else if (!/^[a-zA-Z0-9 ]*$/.test(value)) {
-    console.log('not matching regEx');
     error = 'Use only letters, numbers or spaces.';
+  } else if (!/^\S(.*\S)?$/.test(value)) {
+    error = "Team name can't start or end with a space.";
   }
   return error;
 }
